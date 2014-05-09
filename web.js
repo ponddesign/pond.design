357063b3a955ccb35a8fee8b7e0b13199389b34d
Chris Pond <christopher.pond@endurance.com>
2014-05-08 20:33:15 -0600

    setting up node server for one page app server

--- web.js
+++ web.js
@@ -1,15 +1,30 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+'use strict';
 
-app.use(logfmt.requestLogger());
+var path     = require('path'),
+    express  = require('express'),
+    partials = require('express-partials');
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+var app = module.exports = express();
+
+//== Configurations
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname + '/public')))
+app.use('/assets', express.static(path.join(__dirname + '/assets')))
+
+//== Middlware
+
+app.use(partials())
+
+// app.use(logfmt.requestLogger());
+
+app.all('/', function (req, res) {
+  res.render('www/index.ejs');
 });
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port %d (%s)', app.get('port'), app.get('env'))
 });
