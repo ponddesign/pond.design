8499a0f0f7f6a57384dbb06ba3a463d328c1573b
Christopher Pond <chris@ponddesign.me>
2017-01-31 18:17:37 -0600

    force -www

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), (req, res) => {
+  // enforce www
+  if (req && req.headers && req.headers.host.match(/^www/) !== null ) {
+    res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
+  }
   console.log('Node app is running on port', app.get('port'));
 });
