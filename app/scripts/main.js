546e39440c10f6b4bf1cbbe788c2f157aa08b841
Christopher Pond <chris@ponddesign.me>
2017-01-31 18:34:04 -0600

    converted functions to es6 syntax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,11 @@
-(function() {
+(() => {
   var contactHeight, manageContact, transitionTime;
 
   contactHeight = $('.contact').height() - 5;
 
   transitionTime = 200;
 
-  manageContact = function(height, speed) {
+  manageContact = (height, speed) => {
     if ($(document.body).hasClass('show-hire-me')) {
       $(document.body).animate({
         marginTop: height
@@ -23,35 +23,35 @@
     }
   };
 
-  jQuery(function() {
+  jQuery(() => {
     var viewer;
     $('.contact').css('top', 0 - contactHeight).show();
-    $('.hire-me').click(function(e) {
+    $('.hire-me').click((e) => {
       e.preventDefault;
       $(document.body).toggleClass('show-hire-me');
       return manageContact(contactHeight, transitionTime);
     });
-    $(window).resize(function() {
+    $(window).resize(() => {
       contactHeight = $('.contact').height() - 5;
       return manageContact(contactHeight, 0);
     });
-    $('.projects li a').click(function(e) {
+    $('.projects li a').click((e) => {
       return e.preventDefault();
     });
     viewer = {
       node: $('.sites .viewer'),
-      show: function() {
+      show: () => {
         return this.node.show();
       },
-      hide: function() {
+      hide: () => {
         return this.node.hide();
       }
     };
-    $('.projects > li > a').click(function(e) {
+    $('.projects > li > a').click((e) => {
       e.preventDefault();
       return viewer.show();
     });
-    return $('a.close-viewer').click(function(e) {
+    return $('a.close-viewer').click((e) => {
       e.preventDefault();
       return viewer.hide();
     });
