9b0dce5aa23d6843530cf79ce3a28e775fff4114
Christopher Pond <chris@pond.design>
2023-07-20 15:56:15 -0600

    Add-tailwind-support (#13)
    * add tailwind
    
    * add root
    
    * - add dynamic copyright year
    - add Noto Sans
    - update vertical height to fit logo and copyright

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import dayjs from "dayjs";
 
 import logoText from "../assets/logo-text.svg";
 
@@ -6,17 +7,21 @@ export const meta: V2_MetaFunction = () => {
   return [{ title: "Pond Design" }];
 };
 
+const currentYear = dayjs().format("YYYY");
+
 export default function Index() {
   return (
-    <>
-      <header className="min-h-screen flex flex-col items-center justify-center">
+    <div>
+      <header className="container mx-auto min-h-[90vh] flex flex-col items-center justify-center">
         <h1>
           <img src={logoText} alt="Pond Design" />
         </h1>
       </header>
-      <footer className="text-center text-zinc-600 py-5">
-        <p>Copyright 2023</p>
+      <footer className="container mx-auto text-zinc-600 py-5">
+        <p className="text-center">
+          Copyright &copy; {currentYear} Pond Design. All rights reserved.
+        </p>
       </footer>
-    </>
+    </div>
   );
 }
