8de56bae3c2f24ca1065a9549334d6425ba2b200
Christopher Pond <chris@pond.design>
2023-07-20 12:47:22 -0600

    add tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,21 +1,22 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
+import logoText from "../assets/logo-text.svg";
+
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Pond Design" }];
 };
 
 export default function Index() {
   return (
-    <div>
-      <header>
-        <h1>Pond Design</h1>
+    <>
+      <header className="min-h-screen flex flex-col items-center justify-center">
+        <h1>
+          <img src={logoText} alt="Pond Design" />
+        </h1>
       </header>
-      <main>
-        <p>Under construction.</p>
-      </main>
-      <footer>
+      <footer className="text-center text-zinc-600 py-5">
         <p>Copyright 2023</p>
       </footer>
-    </div>
+    </>
   );
 }
