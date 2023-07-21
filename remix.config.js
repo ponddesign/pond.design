1dde6a4891f77ccf2aadbab4c40e2ae26f5781e5
Christopher Pond <chris@pond.design>
2023-07-21 17:02:28 -0600

    adopt v2 flags

--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   tailwind: true,
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
+  serverModuleFormat: "cjs", // enforce for vercel support
   future: {
     v2_errorBoundary: true,
+    v2_headers: true,
+    v2_dev: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
