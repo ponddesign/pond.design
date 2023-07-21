1a8c2833971964b2bcde1e5e1f98203b2db99760
Christopher Pond <chris@pond.design>
2023-07-21 07:54:20 -0600

    separate template modules

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import logoText from "../../assets/logo-text.svg";
+
+export default function Header() {
+  return (
+    <header className="container mx-auto min-h-[30vh] flex flex-col items-center justify-center">
+      <h1>
+        <img src={logoText} alt="Pond Design" />
+      </h1>
+    </header>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,27 +1,17 @@
 import type { V2_MetaFunction } from "@remix-run/node";
-import dayjs from "dayjs";
 
-import logoText from "../assets/logo-text.svg";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Pond Design" }];
 };
 
-const currentYear = dayjs().format("YYYY");
-
 export default function Index() {
   return (
     <div>
-      <header className="container mx-auto min-h-[90vh] flex flex-col items-center justify-center">
-        <h1>
-          <img src={logoText} alt="Pond Design" />
-        </h1>
-      </header>
-      <footer className="container mx-auto text-zinc-600 py-5">
-        <p className="text-center">
-          Copyright &copy; {currentYear} Pond Design. All rights reserved.
-        </p>
-      </footer>
+      <Header />
+      <Footer />
     </div>
   );
 }
