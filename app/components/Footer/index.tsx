ebe451960edb4f290db5fbd4f44213a5cc94bc22
Christopher Pond <chris@pond.design>
2023-07-29 17:00:21 -0600

    add mstdn rel

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -5,8 +5,13 @@ const currentYear = dayjs().format("YYYY");
 export default function Footer() {
   return (
     <footer className="text-primary-400 container mx-auto py-5">
-      <p className="text-center">
+      <p className="copyright text-center">
         Copyright &copy; {currentYear} Pond Design. All rights reserved.
+      </p>
+      <p className="social hidden">
+        <a rel="me" href="https://mstdn.social/@ponddesign">
+          Mastodon
+        </a>
       </p>
     </footer>
   );
