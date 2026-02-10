9497f9ddc2a0f75d21bf757b89221c1a3ac462b0
Christopher Pond <chris@pond.design>
2026-02-10 13:05:27 -0700

    Update Remix scripts and deps

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    remix({
+      ignoredRouteFiles: ['**/.*'],
+    }),
+  ],
+});
