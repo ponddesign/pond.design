101104c1394d4c591192072fba84a04fe6993820
Christopher Pond <chris@pond.design>
2026-02-10 15:30:39 -0700

    Fix duplicate key and build

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from '@react-router/dev/config';
+import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   appDirectory: 'app',
   ssr: true,
   serverModuleFormat: 'cjs',
+  presets: [vercelPreset()],
 } satisfies Config;
